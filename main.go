7136b5b1fca28d9b0dad3d033dc44478a932ca9d
nitingupta8876 <63221381+nitingupta8876@users.noreply.github.com>
2023-02-13 21:54:35 +0530

    Initia Commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"GoTraining/mypackage"
+)
+
+func init() {
+	fmt.Println("1st Main init")
+}
+
+func main() {
+	fmt.Println("Hello, Modules!")
+
+	//currTime := time.Now()
+	currTime := time.Date(2023, 2, 13, 9, 24, 30, 45, time.Local)
+
+	fmt.Println("The time is ", currTime)
+
+	mypackage.PrintHello()
+}
+
+func init() {
+	fmt.Println("2nd Main init")
+}
